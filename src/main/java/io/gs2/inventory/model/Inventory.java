c2152ce210245ab0a8fbae845b9558de16576a2f
gs2-ci <ci@gs2.io>
2022-06-28 13:06:25 +0000

    783325d448cbd2b0613a129dc208a2dde5a9f13c

--- src/main/java/io/gs2/inventory/model/Inventory.java
+++ src/main/java/io/gs2/inventory/model/Inventory.java
@@ -36,6 +36,7 @@ public class Inventory implements IModel, Serializable, Comparable<Inventory> {
 	private Integer currentInventoryMaxCapacity;
 	private Long createdAt;
 	private Long updatedAt;
+	private Long revision;
 	public String getInventoryId() {
 		return inventoryId;
 	}
@@ -106,6 +107,16 @@ public class Inventory implements IModel, Serializable, Comparable<Inventory> {
 		this.updatedAt = updatedAt;
 		return this;
 	}
+	public Long getRevision() {
+		return revision;
+	}
+	public void setRevision(Long revision) {
+		this.revision = revision;
+	}
+	public Inventory withRevision(Long revision) {
+		this.revision = revision;
+		return this;
+	}
 
     public static Inventory fromJson(JsonNode data) {
         if (data == null) {
@@ -118,7 +129,8 @@ public class Inventory implements IModel, Serializable, Comparable<Inventory> {
             .withCurrentInventoryCapacityUsage(data.get("currentInventoryCapacityUsage") == null || data.get("currentInventoryCapacityUsage").isNull() ? null : data.get("currentInventoryCapacityUsage").intValue())
             .withCurrentInventoryMaxCapacity(data.get("currentInventoryMaxCapacity") == null || data.get("currentInventoryMaxCapacity").isNull() ? null : data.get("currentInventoryMaxCapacity").intValue())
             .withCreatedAt(data.get("createdAt") == null || data.get("createdAt").isNull() ? null : data.get("createdAt").longValue())
-            .withUpdatedAt(data.get("updatedAt") == null || data.get("updatedAt").isNull() ? null : data.get("updatedAt").longValue());
+            .withUpdatedAt(data.get("updatedAt") == null || data.get("updatedAt").isNull() ? null : data.get("updatedAt").longValue())
+            .withRevision(data.get("revision") == null || data.get("revision").isNull() ? null : data.get("revision").longValue());
     }
 
     public JsonNode toJson() {
@@ -131,6 +143,7 @@ public class Inventory implements IModel, Serializable, Comparable<Inventory> {
                 put("currentInventoryMaxCapacity", getCurrentInventoryMaxCapacity());
                 put("createdAt", getCreatedAt());
                 put("updatedAt", getUpdatedAt());
+                put("revision", getRevision());
             }}
         );
     }
@@ -151,6 +164,7 @@ public class Inventory implements IModel, Serializable, Comparable<Inventory> {
         result = prime * result + ((this.currentInventoryMaxCapacity == null) ? 0 : this.currentInventoryMaxCapacity.hashCode());
         result = prime * result + ((this.createdAt == null) ? 0 : this.createdAt.hashCode());
         result = prime * result + ((this.updatedAt == null) ? 0 : this.updatedAt.hashCode());
+        result = prime * result + ((this.revision == null) ? 0 : this.revision.hashCode());
 		return result;
 	}
 
@@ -196,6 +210,11 @@ public class Inventory implements IModel, Serializable, Comparable<Inventory> {
 		if (updatedAt == null) {
 			return other.updatedAt == null;
 		} else if (!updatedAt.equals(other.updatedAt)) {
+			return false;
+		}
+		if (revision == null) {
+			return other.revision == null;
+		} else if (!revision.equals(other.revision)) {
 			return false;
 		}
 		return true;
