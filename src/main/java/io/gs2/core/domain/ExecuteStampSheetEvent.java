b59b84c8159ae5e2c312f493c20ac5d913887aca
gs2-ci <ci@gs2.io>
2023-07-03 09:37:56 +0000

    7cdf772aeab71c05b49ee532f114790d687181e9

--- src/main/java/io/gs2/core/domain/ExecuteStampSheetEvent.java
+++
@@ -1,5 +0,0 @@
-package io.gs2.core.domain;
-
-public interface ExecuteStampSheetEvent {
-    void onEvent(CacheDatabase cache, String action, String request, String result);
-}
--- src/main/java/io/gs2/core/domain/ExecuteStampTaskEvent.java
+++
@@ -1,5 +0,0 @@
-package io.gs2.core.domain;
-
-public interface ExecuteStampTaskEvent {
-    void onEvent(CacheDatabase cache, String action, String request, String result);
-}
